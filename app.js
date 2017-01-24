3ebde683e00062a3f3e8732fe8251ea567e030e2
Debby ji <jasmine820921@gmail.com>
2017-01-25 01:54:07 +0800

    2017/01/25

---
+++ app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  // $(".nav li a").click(function(){
+  //   alert('hi');
+  // });
+
+  $('.nav li a').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
