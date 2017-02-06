9d2f67476aab198d179a44a359cbbd0b1c692564
Debby ji <jasmine820921@gmail.com>
2017-02-06 21:00:30 +0800

    2017/02/06 21:00

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  $('.nav li a').click(function() {
+  $('.navbar li a').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -13,9 +13,58 @@ $(document).ready(function() {
     }
   });
 
-  $(window).scroll(function(){
-    $('.nav').addClass("nav_up");
+  var navbarHeight = $(".navbar").height();
+  var lastScrollTop = 0;
+  var headerHeight = $(".section.home").height();
 
-    // console.log();
+  $(window).scroll(function(){
+    hasScrolled();
   });
+
+  function hasScrolled() {
+    var st = $(this).scrollTop(); //滑動的距離相對於最頂端
+    console.log("st: " + st);
+    console.log("nav: " + navbarHeight);
+    // console.log("lastScrollTop: " + lastScrollTop);
+    console.log("headerHeight: " + headerHeight);
+
+    if(st > (headerHeight - navbarHeight)){
+      // nav 出現
+      $("nav").removeClass("navbar-up");
+    } else {
+      // nav 隱藏
+      $("nav").addClass("navbar-up");
+    }
+    lastScrollTop = st;
+  }
+
+var today = new Date();
+var start_year = "2016";
+var star_month = "8";
+var month = today.getMonth()+1;
+var year = today.getFullYear();
+var diff = (month - star_month) + 12*(year - start_year);
+var diff_year = Math.floor(diff/12);
+
+if(month - star_month < 0){
+  if(diff > 12){
+    var diff_month = diff - diff_year*12;
+  } else {
+    diff_month = diff;
+  }
+} else {
+  diff_month = month - star_month;
+}
+
+$(".self_time").html(diff_year + "年" + diff_month + "個月。");
+// console.log(diff + " " +diff_year + "年" + diff_month + "個月");
+
+
+$(".hamburger_menu").click(function() {
+  $(".navbar ul").slideToggle("show");
+  $(".hamburger").toggleClass("hamburger_exit");
+  console.log("hi");
+});
+
+
 });
